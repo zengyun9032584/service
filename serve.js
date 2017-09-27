3a5691f597a691697b3993eb17d0d528b7d1f239
yun zeng <yun zeng>
2017-09-27 22:41:25 +0800

    后端服务允许跨域访问
    Signed-off-by: yun zeng <yun zeng>

--- serve.js
+++ serve.js
@@ -6,16 +6,20 @@ let app = express();
 let fs = require("fs");
 
 app.get('/list', function (req, res) {
-   fs.readFile( __dirname + "/assets/morse/" + "morse.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  fs.readFile(__dirname + "/assets/morse/" + "morse.json", 'utf8', function (err, data) {
+    console.log(data);
+    res.end(data);
+  });
 })
 
 let server = app.listen(8081, function () {
   // 获取端口
   let port = server.address().port
 
-  console.log("应用实例，访问地址为 http://localhost:%s",port)
+  console.log("应用实例，访问地址为 http://localhost:%s", port)
 
 })
